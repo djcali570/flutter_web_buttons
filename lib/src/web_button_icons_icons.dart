d0a019d6e64eccf959a1a27c0db55179dff61976
djcali570 <djcali570@gmail.com>
2022-02-14 00:57:56 -0500

    Added social icon config file

---
+++ lib/src/web_button_icons_icons.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/widgets.dart';
+
+class WebButtonIcons {
+  WebButtonIcons._();
+
+  static const _kFontFam = 'WebButtonIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData instagram_square = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData twitter_square = IconData(0xf081, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData facebook_square = IconData(0xf082, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData linkedin = IconData(0xf08c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData twitter = IconData(0xf099, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData facebook = IconData(0xf09a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData github = IconData(0xf09b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pinterest = IconData(0xf0d2, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pinterest_square = IconData(0xf0d3, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData linkedin_in = IconData(0xf0e1, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData github_alt = IconData(0xf113, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData youtube = IconData(0xf167, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData instagram = IconData(0xf16d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData vimeo_square = IconData(0xf194, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData soundcloud = IconData(0xf1be, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pinterest_p = IconData(0xf231, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData vimeo_v = IconData(0xf27d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData snapchat = IconData(0xf2ab, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData snapchat_ghost = IconData(0xf2ac, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData snapchat_square = IconData(0xf2ad, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData facebook_f = IconData(0xf39e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData youtube_square = IconData(0xf431, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
