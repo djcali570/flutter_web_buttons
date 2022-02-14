be66e065cc0fe5b0aa129d950a58d016945cd698
djcali570 <djcali570@gmail.com>
2022-02-14 15:04:53 -0500

    updated to include social icon constructor and method to return social button

--- lib/src/web_button.dart
+++ lib/src/web_button.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:web_buttons/src/clippers.dart';
+import 'package:web_buttons/src/icon_config.dart';
 import 'package:web_buttons/src/web_button_optional_fields.dart';
 import 'package:web_buttons/src/enums.dart';
 
 // ignore: must_be_immutable
 class WebButton extends StatefulWidget {
-  final VoidCallback onPressed;
-  final String text;
-  final WebButtonOptionalFields optionalFields;
+  VoidCallback? onPressed;
+  String? text;
+  WebButtonOptionalFields? webButtonOptionalFields;
   Duration? animationDuration;
   Color? backgroundAnimatedColor;
   Color? textAnimatedColor;
+  WebButtonSocialIcon? webButtonSocialIcon;
 
   /// Button Constructors
   WebButton.simple(
     this.text, {
     Key? key,
     required this.onPressed,
-    required this.optionalFields,
+    required this.webButtonOptionalFields,
   })  : _buttonType = WebButtonList.simple,
         super(key: key);
 
@@ -25,7 +27,7 @@ class WebButton extends StatefulWidget {
     this.text, {
     Key? key,
     required this.onPressed,
-    required this.optionalFields,
+    required this.webButtonOptionalFields,
     this.animationDuration,
   })  : _buttonType = WebButtonList.textScroll,
         super(key: key);
@@ -34,7 +36,7 @@ class WebButton extends StatefulWidget {
     this.text, {
     Key? key,
     required this.onPressed,
-    required this.optionalFields,
+    required this.webButtonOptionalFields,
     this.backgroundAnimatedColor,
     this.animationDuration,
   })  : _buttonType = WebButtonList.backgroundColorChange,
@@ -44,7 +46,7 @@ class WebButton extends StatefulWidget {
     this.text, {
     Key? key,
     required this.onPressed,
-    required this.optionalFields,
+    required this.webButtonOptionalFields,
     this.textAnimatedColor,
     this.animationDuration,
   })  : _buttonType = WebButtonList.textColorChange,
@@ -54,7 +56,7 @@ class WebButton extends StatefulWidget {
     this.text, {
     Key? key,
     required this.onPressed,
-    required this.optionalFields,
+    required this.webButtonOptionalFields,
     this.animationDuration,
   })  : _buttonType = WebButtonList.raiseText,
         super(key: key);
@@ -63,14 +65,24 @@ class WebButton extends StatefulWidget {
     this.text, {
     Key? key,
     required this.onPressed,
-    required this.optionalFields,
+    required this.webButtonOptionalFields,
     this.textAnimatedColor,
     this.backgroundAnimatedColor,
     this.animationDuration,
   })  : _buttonType = WebButtonList.backgroundFill,
         super(key: key);
 
+  WebButton.socialIcon({
+    Key? key,
+    required this.webButtonSocialIcon,
+    required this.onPressed,
+    required this.webButtonOptionalFields,
+    this.animationDuration,
+  })  : _buttonType = WebButtonList.socialIcon,
+        super(key: key);
+
   /// Button Type variable thats gets assigned in the constructor.
+
   final WebButtonList _buttonType;
 
   @override
@@ -110,22 +122,23 @@ class _WebButtonState extends State<WebButton>
 
     ///Animations
     _backgroundColorAnimation = ColorTween(
-            begin: widget.optionalFields.buttonBackgroundColor ?? Colors.pink,
+            begin: widget.webButtonOptionalFields!.buttonBackgroundColor ?? Colors.pink,
             end: widget.backgroundAnimatedColor ?? Colors.pink[300])
         .animate(curvedAnimation);
 
     _textColorAnimation = ColorTween(
-            begin: widget.optionalFields.textColor ?? Colors.white,
+            begin: widget.webButtonOptionalFields!.textColor ?? Colors.white,
             end: widget.textAnimatedColor ?? Colors.white70)
         .animate(curvedAnimation);
     _textColorAnimationNoCurve = ColorTween(
-            begin: widget.optionalFields.textColor ?? Colors.pink,
+            begin: widget.webButtonOptionalFields!.textColor ?? Colors.pink,
             end: widget.textAnimatedColor ?? Colors.white)
         .animate(_controller);
     _raiseTextAnimation =
         Tween<double>(begin: 0.0, end: -4).animate(curvedAnimation);
     _backgroundFill = Tween<double>(
-            begin: 0, end: widget.optionalFields.buttonWidth ?? double.infinity)
+            begin: 0,
+            end: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity)
         .animate(curvedAnimation);
 
     /// Sequence Animations
@@ -133,12 +146,12 @@ class _WebButtonState extends State<WebButton>
       TweenSequenceItem<double>(
           tween: Tween<double>(
             begin: 0.0,
-            end: -widget.optionalFields.buttonHeight! / 3,
+            end: -widget.webButtonOptionalFields!.buttonHeight! / 3,
           ),
           weight: 50),
       TweenSequenceItem<double>(
           tween: Tween<double>(
-            begin: widget.optionalFields.buttonHeight! / 3,
+            begin: widget.webButtonOptionalFields!.buttonHeight! / 3,
             end: 0.0,
           ),
           weight: 50),
@@ -166,7 +179,7 @@ class _WebButtonState extends State<WebButton>
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: widget.optionalFields.buttonPadding!,
+      padding: widget.webButtonOptionalFields!.buttonPadding!,
 
       /// A mouse region is used here so that on hover you can start and reverse the animation.
       child: MouseRegion(
@@ -197,7 +210,7 @@ class _WebButtonState extends State<WebButton>
   }
 
   /// Based on the constructor used, return the associated button.
-  Widget getButton() {
+  getButton() {
     switch (widget._buttonType) {
       case WebButtonList.simple:
         return getSimpleButton();
@@ -211,6 +224,8 @@ class _WebButtonState extends State<WebButton>
         return getRaiseTextButton();
       case WebButtonList.backgroundFill:
         return getBackgroundFillButton();
+      case WebButtonList.socialIcon:
+        return getSocialButton(WebButtonIcon.getSocialIcon(widget.webButtonSocialIcon!));
       default:
         return const SizedBox();
     }
@@ -218,9 +233,9 @@ class _WebButtonState extends State<WebButton>
 
   /// Button Types that get returned to the gesture detector.
   getSimpleButton() => Container(
-        width: widget.optionalFields.buttonWidth ?? double.infinity,
-        height: widget.optionalFields.buttonHeight!,
-        decoration: widget.optionalFields.eliminateDecoration!
+        width: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity,
+        height: widget.webButtonOptionalFields!.buttonHeight!,
+        decoration: widget.webButtonOptionalFields!.eliminateDecoration!
             ? null
             : standardBoxDecoration(),
         child: Align(
@@ -229,14 +244,14 @@ class _WebButtonState extends State<WebButton>
               type: MaterialType.transparency,
               textStyle: standardTextStyle(),
               child: Text(
-                widget.text,
+                widget.text!,
               ),
             )),
       );
   getTextScrollButton() => Container(
-        width: widget.optionalFields.buttonWidth ?? double.infinity,
-        height: widget.optionalFields.buttonHeight!,
-        decoration: widget.optionalFields.eliminateDecoration!
+        width: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity,
+        height: widget.webButtonOptionalFields!.buttonHeight!,
+        decoration: widget.webButtonOptionalFields!.eliminateDecoration!
             ? null
             : standardBoxDecoration(),
         child: Align(
@@ -255,7 +270,7 @@ class _WebButtonState extends State<WebButton>
                             child: Opacity(
                               opacity: _textScrollOpacityAnimation.value,
                               child: Text(
-                                widget.text,
+                                widget.text!,
                               ),
                             ),
                           );
@@ -266,8 +281,8 @@ class _WebButtonState extends State<WebButton>
   getBackgroundColorChangeButton() => AnimatedBuilder(
         animation: _backgroundColorAnimation,
         builder: ((context, child) => Container(
-              width: widget.optionalFields.buttonWidth ?? double.infinity,
-              height: widget.optionalFields.buttonHeight,
+              width: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity,
+              height: widget.webButtonOptionalFields!.buttonHeight,
               decoration: BoxDecoration(
                 color: _backgroundColorAnimation.value,
                 borderRadius: standardBorderRadius(),
@@ -280,7 +295,7 @@ class _WebButtonState extends State<WebButton>
                   type: MaterialType.transparency,
                   textStyle: standardTextStyle(),
                   child: Text(
-                    widget.text,
+                    widget.text!,
                   ),
                 ),
               ),
@@ -289,8 +304,8 @@ class _WebButtonState extends State<WebButton>
   getTextColorChangeButton() => AnimatedBuilder(
         animation: _textColorAnimation,
         builder: ((context, child) => Container(
-              width: widget.optionalFields.buttonWidth ?? double.infinity,
-              height: widget.optionalFields.buttonHeight,
+              width: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity,
+              height: widget.webButtonOptionalFields!.buttonHeight,
               decoration: standardBoxDecoration(),
               child: Align(
                 alignment: Alignment.center,
@@ -298,20 +313,20 @@ class _WebButtonState extends State<WebButton>
                   type: MaterialType.transparency,
                   textStyle: TextStyle(
                     color: _textColorAnimation.value,
-                    fontFamily: widget.optionalFields.fontFamily ?? '',
-                    fontSize: widget.optionalFields.fontSize ?? 16,
+                    fontFamily: widget.webButtonOptionalFields!.fontFamily ?? '',
+                    fontSize: widget.webButtonOptionalFields!.fontSize ?? 16,
                   ),
                   child: Text(
-                    widget.text,
+                    widget.text!,
                   ),
                 ),
               ),
             )),
       );
   getRaiseTextButton() => Container(
-        width: widget.optionalFields.buttonWidth ?? double.infinity,
-        height: widget.optionalFields.buttonHeight,
-        decoration: widget.optionalFields.eliminateDecoration!
+        width: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity,
+        height: widget.webButtonOptionalFields!.buttonHeight,
+        decoration: widget.webButtonOptionalFields!.eliminateDecoration!
             ? null
             : standardBoxDecoration(),
         child: Align(
@@ -324,24 +339,24 @@ class _WebButtonState extends State<WebButton>
                       type: MaterialType.transparency,
                       textStyle: standardTextStyle(),
                       child: Text(
-                        widget.text,
+                        widget.text!,
                       ),
                     ),
                   )),
         ),
       );
   getBackgroundFillButton() => SizedBox(
-        width: widget.optionalFields.buttonWidth ?? double.infinity,
-        height: widget.optionalFields.buttonHeight,
+        width: widget.webButtonOptionalFields!.buttonWidth ?? double.infinity,
+        height: widget.webButtonOptionalFields!.buttonHeight,
 
         /// A clip path is used so the fill color does not show when using a radius button.
         child: ClipPath(
-          clipper: PillClipper(widget.optionalFields.buttonRadius ?? 0),
+          clipper: PillClipper(widget.webButtonOptionalFields!.buttonRadius ?? 0),
           child: Stack(
             children: [
               Container(
                 decoration: BoxDecoration(
-                  color: widget.optionalFields.buttonBackgroundColor ??
+                  color: widget.webButtonOptionalFields!.buttonBackgroundColor ??
                       Colors.pink[100],
                   border: standardBorder(),
                 ),
@@ -371,11 +386,11 @@ class _WebButtonState extends State<WebButton>
                         type: MaterialType.transparency,
                         textStyle: TextStyle(
                           color: _textColorAnimationNoCurve.value,
-                          fontFamily: widget.optionalFields.fontFamily ?? '',
-                          fontSize: widget.optionalFields.fontSize ?? 16,
+                          fontFamily: widget.webButtonOptionalFields!.fontFamily ?? '',
+                          fontSize: widget.webButtonOptionalFields!.fontSize ?? 16,
                         ),
                         child: Text(
-                          widget.text,
+                          widget.text!,
                         ),
                       )),
                 ),
@@ -384,49 +399,52 @@ class _WebButtonState extends State<WebButton>
           ),
         ),
       );
+  static getSocialButton(IconData icon) => SizedBox(
+        child: Icon(icon),
+      );
 
   /// Default decorations are used by multiple buttons so these eliminate repeated code.
   standardBoxDecoration() => BoxDecoration(
-        color: widget.optionalFields.eliminateDecoration!
+        color: widget.webButtonOptionalFields!.eliminateDecoration!
             ? Colors.transparent
-            : widget.optionalFields.buttonBackgroundColor ?? Colors.pink,
+            : widget.webButtonOptionalFields!.buttonBackgroundColor ?? Colors.pink,
         borderRadius: BorderRadius.all(
-            Radius.circular(widget.optionalFields.buttonRadius ?? 0)),
-        border: widget.optionalFields.buttonBorderColor == null
+            Radius.circular(widget.webButtonOptionalFields!.buttonRadius ?? 0)),
+        border: widget.webButtonOptionalFields!.buttonBorderColor == null
             ? const Border.fromBorderSide(BorderSide.none)
             : Border.all(
-                color: widget.optionalFields.buttonBorderColor!,
-                width: widget.optionalFields.buttonBorderWidth ?? 1.0),
-        boxShadow: widget.optionalFields.boxShadowColor != null
+                color: widget.webButtonOptionalFields!.buttonBorderColor!,
+                width: widget.webButtonOptionalFields!.buttonBorderWidth ?? 1.0),
+        boxShadow: widget.webButtonOptionalFields!.boxShadowColor != null
             ? [
                 BoxShadow(
-                  color: widget.optionalFields.boxShadowColor!,
-                  spreadRadius: widget.optionalFields.spreadRadius ?? -5,
-                  blurRadius: widget.optionalFields.blurRadius ?? 7,
-                  offset: widget.optionalFields.boxShadowOffset,
+                  color: widget.webButtonOptionalFields!.boxShadowColor!,
+                  spreadRadius: widget.webButtonOptionalFields!.spreadRadius ?? -5,
+                  blurRadius: widget.webButtonOptionalFields!.blurRadius ?? 7,
+                  offset: widget.webButtonOptionalFields!.boxShadowOffset,
                 )
               ]
             : null,
       );
   standardBorderRadius() => BorderRadius.all(
-      Radius.circular(widget.optionalFields.buttonRadius ?? 0));
-  standardBorder() => widget.optionalFields.buttonBorderColor == null
+      Radius.circular(widget.webButtonOptionalFields!.buttonRadius ?? 0));
+  standardBorder() => widget.webButtonOptionalFields!.buttonBorderColor == null
       ? const Border.fromBorderSide(BorderSide.none)
       : Border.all(
-          color: widget.optionalFields.buttonBorderColor!,
-          width: widget.optionalFields.buttonBorderWidth ?? 1.0);
+          color: widget.webButtonOptionalFields!.buttonBorderColor!,
+          width: widget.webButtonOptionalFields!.buttonBorderWidth ?? 1.0);
   standardTextStyle() => TextStyle(
-        color: widget.optionalFields.textColor ?? Colors.white,
-        fontFamily: widget.optionalFields.fontFamily ?? '',
-        fontSize: widget.optionalFields.fontSize ?? 16,
+        color: widget.webButtonOptionalFields!.textColor ?? Colors.white,
+        fontFamily: widget.webButtonOptionalFields!.fontFamily ?? '',
+        fontSize: widget.webButtonOptionalFields!.fontSize ?? 16,
       );
-  standardBoxShadow() => widget.optionalFields.boxShadowColor != null
+  standardBoxShadow() => widget.webButtonOptionalFields!.boxShadowColor != null
       ? [
           BoxShadow(
-            color: widget.optionalFields.boxShadowColor!,
-            spreadRadius: widget.optionalFields.spreadRadius ?? -5,
-            blurRadius: widget.optionalFields.blurRadius ?? 7,
-            offset: widget.optionalFields.boxShadowOffset,
+            color: widget.webButtonOptionalFields!.boxShadowColor!,
+            spreadRadius: widget.webButtonOptionalFields!.spreadRadius ?? -5,
+            blurRadius: widget.webButtonOptionalFields!.blurRadius ?? 7,
+            offset: widget.webButtonOptionalFields!.boxShadowOffset,
           )
         ]
       : null;
